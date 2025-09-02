e0b1cca3c4ab83fcef66805c470f0fcc4a29ae64
Xenial1024 <marko1024@seznam.cz>
2025-09-02 17:30:21 +0200

    Improve validation

--- ChatBot/Validators/CreateSettingsValidator.cs
+++ ChatBot/Validators/CreateSettingsValidator.cs
@@ -12,8 +12,8 @@ namespace ChatBot.Validators
                 .WithMessage("Kreatywność może tylko przyjmować wartości z zakresu od 0 do 1.");
 
             RuleFor(x => x.MaxTokens)
-                .GreaterThan(0)
-                .WithMessage("Liczba tokenów musi być dodatnia.");
+                .InclusiveBetween(1, 16384)
+                .WithMessage("Liczba tokenów musi być z przedziału 1 - 16384.");
         }
     }
 }
