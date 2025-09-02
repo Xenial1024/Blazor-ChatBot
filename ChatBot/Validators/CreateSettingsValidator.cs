26960204945da74335c67705794623354395fe5d
Xenial1024 <marko1024@seznam.cz>
2025-09-02 10:29:13 +0200

    Add validation

---
+++ ChatBot/Validators/CreateSettingsValidator.cs
@@ -0,0 +1,19 @@
+﻿using ChatBot.Models;
+using FluentValidation;
+
+namespace ChatBot.Validators
+{
+    public class CreateSettingsValidator : AbstractValidator <Settings>
+    {
+        public CreateSettingsValidator()
+        {
+            RuleFor(x => x.Creativity)
+                .InclusiveBetween(0, 1)
+                .WithMessage("Kreatywność może tylko przyjmować wartości z zakresu od 0 do 1.");
+
+            RuleFor(x => x.MaxTokens)
+                .GreaterThan(0)
+                .WithMessage("Liczba tokenów musi być dodatnia.");
+        }
+    }
+}
